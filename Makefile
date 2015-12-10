783626522a81a8d2640817dc6880cd960d0b7b0a
kele <damian.bogel@gmail.com>
2015-12-10 22:18:19 +0100

    Makefile updated with tests.

--- Makefile
+++ Makefile
@@ -1,1 +1,7 @@
 CXXFLAGS=-std=c++11 -g
+
+test_graph: test_graph.cpp graph.hpp
+	${CXX} ${CXXFLAGS} test_graph.cpp -o $@
+	
+test_subsets: test_subsets.cpp subsets.hpp
+	${CXX} ${CXXFLAGS} test_subsets.cpp -o $@
--- test_graph.cpp
+++ test_graph.cpp
@@ -3,17 +3,17 @@
 
 int main()
 {
-    Graph g(10);
+    Graph<> g(10);
 
-    g.add_edge(1, 2);
-    g.add_edge(1, 2);
+    g.add_edge({1, 2});
+    g.add_edge({1, 2});
     std::cout << "graph\n" << g << std::endl;
 
-    g.remove_edge(1, 2);
+    g.remove_edge({1, 2});
     std::cout << "graph\n" << g << std::endl;
 
-    g.add_edge(2, 1);
-    g.add_edge(2, 5);
+    g.add_edge({2, 1});
+    g.add_edge({2, 5});
     std::cout << "graph\n" << g << std::endl;
 
 }
