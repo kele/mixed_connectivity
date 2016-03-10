6d9d02b76859555621f046ebef2f3570b55c09e8
kele <damian.bogel@gmail.com>
2016-03-10 18:02:56 +0100

    Added reference wrapper to a mutable object to estd.

--- src/estd.hpp
+++ src/estd.hpp
@@ -17,4 +17,29 @@ void throw_assert(bool condition, std::string s)
         throw std::logic_error(s);
 }
 
+template<class T>
+class mutref
+{
+public:
+    explicit mutref(T & obj)
+        : me(obj)
+    {
+    }
+
+    operator T&()
+    {
+        return me;
+    }
+
+    T* operator->()
+    {
+        return &me;
+    }
+private:
+    T& me;
+};
+
+template<class T>
+mutref<T> mut(T &obj) { return mutref<T>(obj); }
+
 } // namespace estd
