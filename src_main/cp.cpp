1a780504ffc39ff615ef8837f484b38ae641787d
kele <damian.bogel@gmail.com>
2016-03-14 16:50:16 +0100

    Changed name from cp to conn_pairs to avoid confusion. Also, little tweaks and a fix to conn_pairs.

--- src_main/cp.cpp
+++ src_main/cp.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
 #include "connectivity_pairs.hpp"
 #include "load_graph.hpp"
@@ -7,6 +8,7 @@
 
 void print_usage()
 {
+    std::cout << "TODO" << std::endl;
 }
 
 int main(int argc, char *argv[])
@@ -21,32 +23,37 @@ int main(int argc, char *argv[])
     std::ifstream input(input_filename);
     std::string line;
 
-    int line_num = 0;
+    int line_num = 1;
     while (input.good())
     {
+        if (line_num % 1000 == 0)
+            std::cerr << line_num << "...\n";
+
         std::getline(input, line);
         if (line != "")
         {
-            auto g = load_graph(estd::mut(input));
+            std::stringstream liness(line);
+            auto g = load_graph(estd::mut(liness));
             auto edges = g.edges();
 
             // making the graph undirected
             for (const auto &e : edges)
                 g.add_edge({e.stop, e.start});
 
-            std::cout << "Graph #" << line_num << std::endl;
             for (int i = 0; i < g.size(); i++)
                 for (int j = i + 1; j < g.size(); j++)
                 {
+                    std::cout << "Graph #" << line_num << ": ";
                     std::cout << "cp for " << i << " <-> " << j << ": ";
                     for (const auto &cp : get_connectivity_pairs(g, i, j))
                         std::cout << "(" << cp.first << ", " << cp.second << ") ";
                     std::cout << std::endl;
                 }
             std::cout << "--\n";
+
+            line_num++;
         }
 
-        line_num++;
     }
 
     return 0;
