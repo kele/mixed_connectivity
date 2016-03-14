429a2e637240fe5a1f2ffb5f7fc937ccc1a41c96
kele <damian.bogel@gmail.com>
2016-03-14 01:16:37 +0100

    Added a failing test case for mixed paths

--- tst/test_mixed_paths.cpp
+++ tst/test_mixed_paths.cpp
@@ -1,6 +1,9 @@
 #include "catch.hpp"
 
 #include "mixed_paths.hpp"
+#include "extraordinary_graphs.hpp"
+
+#include <iostream> // DEBUG
 
 TEST_CASE("A cycle with two chords", "[mixed_paths]")
 {
@@ -25,4 +28,21 @@ TEST_CASE("A cycle with two chords", "[mixed_paths]")
 
     REQUIRE(res.size() == expected.size());
     REQUIRE(std::equal(res.begin(), res.end(), expected.begin()));
+}
+
+
+TEST_CASE("Mixed path's for Petersen's graph", "[mixed_paths]")
+{
+    SimpleGraph g(10);
+    for (const auto &e : petersen_graph_e)
+    {
+        g.add_edge(e);
+        g.add_edge({e.stop, e.start});
+    }
+
+    // TODO: fix mixed paths, because apparently, it does not work
+    auto mixed_paths_count = get_mixed_paths_count(g, 0, 4);
+
+    for (const auto &cp : mixed_paths_count)
+        std::cerr << cp.first << " " << cp.second << std::endl;
 }
