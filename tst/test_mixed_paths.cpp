d6efc7210769ca149f1041995a22818f0d12abc7
kele <damian.bogel@gmail.com>
2016-04-01 00:22:18 +0200

    Added mixed paths clique test.

--- tst/test_mixed_paths.cpp
+++ tst/test_mixed_paths.cpp
@@ -54,3 +54,23 @@ TEST_CASE("Mixed path's for Petersen's graph", "[mixed_paths]")
     REQUIRE(res.size() == expected.size());
     REQUIRE(std::equal(res.begin(), res.end(), expected.begin()));
 }
+
+TEST_CASE("Mixed paths clique.", "[mixed_paths]")
+{
+    const int GRAPH_SIZE = 7;
+    SimpleGraph g(GRAPH_SIZE);
+    for (int i = 0; i < GRAPH_SIZE; i++)
+        for (int j = i + 1; j < GRAPH_SIZE; j++)
+            g.add_edge({i, j});
+
+    g.remove_edge({0, 1});
+
+    auto res = get_mixed_paths_count(g, 0, 1);
+
+    std::vector<std::pair<int, int>> expected;
+    for (int i = 0; i <= GRAPH_SIZE - 2; i++)
+        expected.push_back({i, GRAPH_SIZE - 2 - i});
+
+    REQUIRE(res.size() == expected.size());
+    REQUIRE(std::equal(res.begin(), res.end(), expected.begin()));
+}
